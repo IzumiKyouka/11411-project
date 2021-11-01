44e3c40720866089bfabda81ceac33fa8c506ceb
Burged-MGL <hongyizh@andrew.cmu.edu>
2021-11-01 00:11:28 -0400

    when-question revision 1

--- stanza_test_when.py
+++ stanza_test_when.py
@@ -1,41 +1,157 @@
 import stanza
 
-article = """Cristiano Ronaldo dos Santos Aveiro GOIH (born 5 February 1985), known as Cristiano Ronaldo, is a Portuguese professional footballer who plays for Spanish club Real Madrid and the Portugal national team. He is a forward and serves as captain for Portugal. By the age of 22, Ronaldo had received Ballon d'Or and FIFA World Player of the Year nominations. The following year, in 2008, he won his first Ballon d'Or and FIFA World Player of the Year awards. He then won the FIFA Ballon d'Or in 2013 and 2014. In September 2015, Ronaldo scored his 500th senior career goal for club and country.
-Often ranked as the best player in the world and rated by some in the sport as the greatest of all time, in 2015 Ronaldo was named the best Portuguese player of all time by the Portuguese Football Federation, during its 100th anniversary celebrations. He is the first player to win four European Golden Shoe awards. With Manchester United and Real Madrid, Ronaldo has won three Premier Leagues, one La Liga, one FA Cup, two Football League Cups, two Copas del Rey, two UEFA Champions Leagues, one UEFA Super Cup and two FIFA Club World Cups."""
+f = open("11X11-Course-Project-Data/set1/a8.txt", "r", encoding="UTF-8")
+article = f.read()
+f.close()
 
-nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
+article = article[:20000]
+
+nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse,constituency')
 
 doc = nlp(article)
 
-for sentence in doc.sentences:
-    for word in sentence.words:
-        if word.deprel == 'root':
-            # print("the root word %s has POS tag upos:%s, xpos:%s" % (word.text, word.upos, word.xpos))
-            root_word = word
-            root_postag = word.upos
-    if root_postag == 'VERB':
+def run():
+    for sentence in doc.sentences:
         for word in sentence.words:
-            if word.head == root_word.id and word.deprel == 'nsubj':
-                subj_word = word
-            if word.upos == 'NUM' and len(word.text) == 4 and word.head == root_word.id:
-                year = int(word.text)
+            if word.deprel == 'root':
+                # print("the root word %s has POS tag upos:%s, xpos:%s" % (word.text, word.upos, word.xpos))
+                root_word = word
+                root_postag = word.upos
+        if root_postag == 'VERB':
+            for word in sentence.words:
+                if word.head == root_word.id and word.deprel == 'nsubj':
+                    subj_word = word
+                if word.upos == 'NUM' and len(word.text) == 4 and word.head == root_word.id:
+                    year = int(word.text)
+        try:
+            if root_word.xpos == 'VBD':
+                try:
+                    verb = root_word.lemma
+                    something = ["%s" % verb]
+                    for word in sentence.words[root_word.id:]:
+                        if word.xpos not in ['PRP$', 'JJ', 'NN', 'IN', 'CC', 'DT', 'NNP']: break
+                        something.append(word.text)
+                    mid = ' '.join(something)
+                    question = ("When did %s " % subj_word.text) + mid + "?"
+                    answer = "In %d" % year
+                    print()
+                    print(question)
+                    print(answer)
+                except:
+                    pass
+        except:
+            pass
+
+
+# this function assumes the given sentence contains a time phrase
+def ask_when(sentence):
+    word_list = ['When', 'did']
+
+    tokens = get_main_info(sentence)
+    if 'year' not in tokens.keys(): return None
+    if 'main_verb_lemma' not in tokens.keys(): return None
+    if 'main_obj' not in tokens.keys(): return None
+
+    word_list.append(tokens['main_subj'])
+    word_list.append(tokens['main_verb_lemma'])
     try:
-        if root_word.xpos == 'VBD':
-            try:
-                verb = root_word.lemma
-                something = ["%s" % verb]
-                for word in sentence.words[root_word.id:]:
-                    if word.xpos not in ['PRP$', 'JJ', 'NN', 'IN', 'CC', 'DT', 'NNP']: break
-                    something.append(word.text)
-                mid = ' '.join(something)
-                question = ("When did %s " % subj_word.text) + mid + "?"
-                answer = "In %d" % year
-                print()
-                print(question)
-                print(answer)
-            except:
-                pass
+        word_list.append(tokens['main_obj'])
     except:
         pass
 
-print()
+    temp = ' '.join(word_list)
+    if temp[-1] == ' ': temp = temp[:-1]
+
+    try:
+        month = tokens['month'] + ' '
+    except:
+        month = ''
+    try:
+        year = tokens['year']
+    except:
+        year = ''
+    answer = " In %s%d." % (month, year)
+
+    return temp + '?' + answer
+
+def find_head(word):
+    return word.head
+
+def find_root(sentence):
+    for word in sentence.words:
+        if word.deprel == 'root': return word
+
+def is_main_verb(word):
+    return word.deprel == 'root' and word.upos == 'VERB' and word.xpos in ['VB', 'VBP', 'VBZ', 'VBD', 'VBG', 'VBN']
+
+def is_month(word):
+    month_set = {'January', 'February', 'March', 'April', 
+                 'May', 'June', 'July', 'August', 
+                 'September', 'October', 'November', 'December'}
+    return word.text in month_set
+
+def is_const_word(part):
+    return len(part.children) == 0
+
+def add_to_list(lst, child):
+    if not is_const_word(child):
+        for g_child in child.children:
+            add_to_list(lst, g_child)
+    else:
+        text = child.label
+        lst.append(text)
+
+def get_main_info(sentence):
+    return_tokens = dict()
+
+    for word in sentence.words:
+        # # identify action
+        if is_main_verb(word):
+            return_tokens['main_verb_lemma'] = word.lemma
+        # if word.deprel == 'aux' and is_main_verb(sentence.words[word.head - 1]):
+        #     return_tokens['aux_verb'] = word
+        # if word.lemma == 'not':
+        #     return_tokens['negation'] = True
+        
+        # # identify agent
+        # if word.deprel == 'nsubj' and is_main_verb(sentence.words[word.head - 1]):
+        #     subject_word = word.text
+
+        # identify time
+        if word.upos == 'NUM' and word.text.isdigit() and len(word.text) == 4:
+            return_tokens['year'] = int(word.text)
+            check_month = sentence.words[word.head - 1]
+            if is_month(check_month) and is_main_verb(sentence.words[check_month.head - 1]):
+                return_tokens['month'] = check_month.text
+    
+    for child in sentence.constituency.children[0].children:
+        if child.label == 'NP':
+            subj_list = []
+            add_to_list(subj_list, child)
+            subject = ' '.join(subj_list)
+            return_tokens['main_subj'] = subject
+        if child.label == 'VP':
+            # verb_list = []
+            obj_list = []
+            for g_child in child.children:
+                if g_child.label == 'VBD':
+                    # add_to_list(verb_list, g_child)
+                    verb = g_child.children[0].label
+                    return_tokens['main_verb'] = verb
+                elif g_child.label == 'NP':
+                    add_to_list(obj_list, g_child)
+                    object = ' '.join(obj_list)
+                    return_tokens['main_obj'] = object
+    
+    return return_tokens
+
+
+for sentence in doc.sentences:
+    temp = ask_when(sentence)
+    if temp is not None:
+        print()
+        print(ask_when(sentence))
+
+    
+    
+
